1a52ea69740bd49fadd93f5bb1922838c00fc21a
Danul De Leon <danul.deleon@gmail.com>
2018-09-06 11:31:47 -0700

    completed the stretch goal

--- kris-rebecca-danul/scripts/articleView.js
+++ kris-rebecca-danul/scripts/articleView.js
@@ -97,14 +97,24 @@ articleView.handleMainNav = function() {
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
-
+  // let showMore = false;
+  $('article a').attr('data-show', 'false');
+  
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
   $('article').on('click', 'a', function (event){
     event.preventDefault();
-    // debugger;
     const selected = $(this).parent();
-    selected.find($('.article-body *:nth-of-type(n+2)')).fadeIn();
+    if ($(this).attr('data-show') === 'false') {
+      selected.find($('.article-body *:nth-of-type(n+2)')).fadeIn();
+      $(this).html('Show Less &rarr;');
+      $(this).attr('data-show', 'true');
+    } else {
+      selected.find($('.article-body *:nth-of-type(n+2)')).hide();
+      $(this).html('Read on &rarr;');
+      $(this).attr('data-show', 'false');
+
+    }
   });
 };
 
