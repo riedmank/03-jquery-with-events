1853cdbb6557772ef6903a9144ce59c0b611d626
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-09-06 10:33:26 -0700

    Add nav event handlers

--- kris-rebecca-danul/scripts/articleView.js
+++ kris-rebecca-danul/scripts/articleView.js
@@ -74,6 +74,21 @@ articleView.handleMainNav = function() {
   // TODO: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
+  $('.tab').on('click', 'a', function(event) {
+    event.preventDefault();
+    const selected = $(this).text();
+
+    if (selected === 'Home') {
+      $('article').fadeIn(750);
+      $('.template').hide();
+      $('.tab-content').fadeIn();
+      $('#about').hide();
+    } else if (selected === 'About') {
+      $('article').hide();
+      $('.tab-content').hide();
+      $('#about').fadeIn(750);
+    }
+  })
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('nav .tab:first').click();
@@ -92,4 +107,9 @@ $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+
+  $('#about').hide()
+  $('article').hide();
+  $('.tab-content').hide();
 })
